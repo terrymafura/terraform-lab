dc70d65d9b40a9f0b3082646dd11bd5351af8eab
Terry Mafura <maffsojah1@gmail.com>
2022-06-09 15:16:51 +0200

    Load balancer for prod_web

--- prod.tf
+++ prod.tf
@@ -10,6 +10,20 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+  tags = {
+    "Terraform" : "true"
+  } 
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-1b"
+  tags = {
+    "Terraform" : "true"
+  } 
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "Allow standard http and https ports inbound and everything outbound"
@@ -58,6 +72,23 @@ resource "aws_eip_association" "prod_web" {
   allocation_id = aws_eip.prod_web.id
 }
 resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
   tags = {
     "Terraform" : "true"
   }
